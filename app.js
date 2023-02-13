3a89b086e245ca7996e659a3a152dd7651f33014
Tiffany Nguyen <trucn0311@gmail.com>
2023-02-13 15:04:03 -0600

    create user-authentication pages

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const session = require('express-session');
+const passport = require('./middlewares/passport-config');
+
+const app = express();
+const port = 3000;
+const userRoutes = require('./routes/user-routes');
+const bodyParser = require('body-parser')
+const flash = require('express-flash');
+
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+
+function setupApp() {
+    app.set('view-engine', 'ejs');
+    app.use(bodyParser.urlencoded({ extended: false }));
+    const sessionConfig = { secret: process.env.SESSION, resave: false, saveUninitialized: true };
+    app.use(session(sessionConfig));
+    app.use(flash());
+    app.use(passport.initialize());
+    app.use(passport.session());
+    app.use('/', userRoutes);
+
+}
+
+setupApp();
+app.listen(port, () =>
+    console.log(`listening on... ${port}`));
