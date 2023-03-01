74739bfe3f4c90ffe59d889b9b3a15f2d2d093e8
Tiffany Nguyen <trucn0311@gmail.com>
2023-02-28 20:25:04 -0600

    test

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 
-
+#testing
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
